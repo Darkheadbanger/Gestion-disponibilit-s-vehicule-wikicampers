34893947e84df04cad563e1faa5ca85794d52062
David Bouhaben <davidbouhaben@yahoo.co.id>
2024-05-09 16:07:13 +0200

    trying to understand router

--- src/Controller/ReicipeController.php
+++ src/Controller/ReicipeController.php
@@ -5,14 +5,25 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class ReicipeController extends AbstractController
 {
-    #[Route('/reicipe', name: 'app_reicipe')]
-    public function index(): Response
+    #[Route('/recette', name: 'recip.index')]
+    public function index(Request $request): Response
     {
         return $this->render('reicipe/index.html.twig', [
             'controller_name' => 'ReicipeController',
+        ]);
+    }
+    #[Route('/recette/{slug}-{id}', name: 'recip.show', requirements: ['id' => '\d+', "slug" => "[a-z0-9\-]+"])]
+    public function show(Request $request, string $slug, int $id): Response
+    {
+        return $this->render('reicipe/show.html.twig', [
+            'controller_name' => 'ReicipeController',
+            'slug' => $slug,
+            'demo' => '<strong>demo</strong>',
+            'id' => $id,
         ]);
     }
 }
